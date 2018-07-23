18f7c3abcd09e3403c8545ed8975d615ba3d4ad2
Philip Kandyba <kandubaphilip@bitbucket.com>
2018-07-23 18:38:52 +0300

    commit

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_session import Session
 from flask_migrate import Migrate
 
+from view import *
+
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://admin:490759Filosof@127.0.0.1:5432/taxi'
